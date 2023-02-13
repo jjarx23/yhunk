5faa094ca3833a7063664c58edf8f80789e9ab7b
iudah <olaolujude@gmail.com>
2023-02-13 17:01:08 +0100

    Added support for merging and splitting memory blocks

--- mem.c
+++ mem.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <time.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "mem.h"
 
 typedef struct blk_ blk_t;
@@ -16,27 +17,33 @@ struct blk_
     char ptr;
 };
 
-#define align(n) (n + sizeof(void *) - 1) & ~(sizeof(void *) - 1)
-#define alloc_size(n) (sizeof(struct head_) + n)
+#define ALIGN(n) (n + sizeof(void *) - 1) & ~(sizeof(void *) - 1)
+#define META_RESIZE(n) (sizeof(struct head_) + n)
+#define MIN_SIZE (META_RESIZE(sizeof(void *)))
+#define CAN_SPLIT(b, sz) ((((blk_t *)b)->head.size - sz) >= MIN_SIZE)
+#define GET_HEAD(b) ((blk_t *)(((char *)b) - sizeof(struct head_)))
+#define GET_SUCCESSIVE_HEAD(b) ((blk_t *)((&((blk_t *)b)->ptr) + b->head.size))
+#define CAN_MERGE(b) (GET_SUCCESSIVE_HEAD(b) == ((blk_t *)b)->head.next && ((blk_t *)b)->head.next->head.in_use == 0)
+
 #define FIRST_FIT 0
 #define BEST_FIT 01
-#define NEXT_FIT 1
+#define NEXT_FIT 0
 
 #define TIMER clock()
-#define TTIME(t) (((double)(clock()-t))/CLOCKS_PER_SEC)
+#define TTIME(t) (((double)(clock() - t)) / CLOCKS_PER_SEC)
 
 blk_t *heap_start = 0;
 blk_t *top;
 blk_t *last_fit = 0;
 
-void *get_mem_chunk(size_t sz)
+static void *get_mem_chunk(size_t sz)
 {
     blk_t *brk = sbrk(0);
     if (sbrk(sz) == (void *)-1)
         return 0;
     return brk;
 }
-void *reuse_block(size_t size)
+static void *reuse_block(size_t size)
 {
     if (FIRST_FIT)
     {
@@ -48,29 +55,29 @@ void *reuse_block(size_t size)
                 b->in_use = 1;
                 return b;
             }
-            b = (struct head_*)b->next;
+            b = (struct head_ *)b->next;
         }
     }
     else if (BEST_FIT)
     {
         struct head_ *b = (struct head_ *)heap_start,
-        *res=0;
+                     *res = 0;
         while (b)
         {
             if (b->size >= size && !b->in_use)
             {
-              if(b->size==size){
-                b->in_use=1;
-                //printf("exact \n");
-                return b;
-              }
-              if(res?res->size>b->size:1)res=b;
+                if (b->size == size)
+                {
+                    b->in_use = 1;
+                    return b;
+                }
+                if (res ? res->size > b->size : 1)
+                    res = b;
             }
-            b = (struct head_*)b->next;
-            //printf("no match\n");
+            b = (struct head_ *)b->next;
         }
-        if(res)res->in_use=1;
-        //printf("%p\n", res);
+        if (res)
+            res->in_use = 1;
         return res;
     }
     else if (NEXT_FIT)
@@ -81,29 +88,39 @@ void *reuse_block(size_t size)
             if (b->size >= size && !b->in_use)
             {
                 b->in_use = 1;
-                last_fit = b->next;//exit(0);
-          //printf("h next[%p]{%zu}\n",last_fit, b->size);
+                last_fit = b->next;
                 return b;
             }
-     b = b->next?b->next:heap_start;
-     //printf("%p->[%p]  %p::%p\n", b, b?b->next:0, last_fit, heap_start);
-     if((b==((struct head_*)heap_start)&&last_fit==0)||b==last_fit){
-       //printf("not found\n");
-       return 0;
-     }
+            b = (struct head_ *)(b->next ? b->next : heap_start);
+            if ((b == ((struct head_ *)heap_start) && last_fit == 0) || ((blk_t *)b) == last_fit)
+            {
+                return 0;
+            }
         }
     }
     return 0;
 }
-void *alloc(size_t sz)
+static blk_t *split(blk_t *b, size_t sz)
 {
-    sz = align(sz);
+    blk_t *bb = (blk_t *)(((char *)(b)) + META_RESIZE(sz));
+    bb->head.size = b->head.size - META_RESIZE(sz);
+    b->head.size = sz;
+    bb->head.next = b->head.next;
+    b->head.next = bb;
+    bb->head.in_use = 0;
+    return bb;
+}
+static void *alloc(size_t sz)
+{
+    sz = ALIGN(sz);
     blk_t *b;
     if ((b = reuse_block(sz)))
     {
+        if (CAN_SPLIT(b, sz))
+            split(b, sz);
         return &b->ptr;
     }
-    b = get_mem_chunk(alloc_size(sz));
+    b = get_mem_chunk(META_RESIZE(sz));
     b->head.in_use = 1;
     b->head.size = sz;
     if (!heap_start)
@@ -114,10 +131,18 @@ void *alloc(size_t sz)
     b->head.next = 0;
     return &b->ptr;
 }
-#define get_head(b) ((blk_t *)(((char *)b) - sizeof(struct head_)))
-void afree(void *b)
+static void merge(blk_t *b)
 {
-    get_head(b)->head.in_use = 0;
+    blk_t *nb = GET_SUCCESSIVE_HEAD(b);
+    b->head.next = nb->head.next;
+    b->head.size = META_RESIZE(nb->head.size);
+}
+static void afree(void *b)
+{
+    blk_t *h = GET_HEAD(b);
+    h->head.in_use = 0;
+    if (CAN_MERGE(h))
+        merge(h);
 }
 int main()
 {
@@ -130,22 +155,24 @@ int main()
     t = TIMER;
     // work here
     int i = 100000;
-    int j=0;
-    void * _[i];
+    int j = 0;
+    void *_[i];
     while (i)
     {
         i--;
-        _[j]=(malloc((100000-i)%83 + 3));
-        if(i%13)
-        free(_[j]);
-        else{
-          j++;
+        _[j] = (malloc((100000 - i) % 83 + 3));
+        if (i % 13)
+            free(_[j]);
+        else
+        {
+            j++;
         }
     }
-while(j){
-  j--;
-  free(_[j]);
-}
+    while (j)
+    {
+        j--;
+        free(_[j]);
+    }
 
     printf(" took %f seconds to execute \n", TTIME(t));
 
@@ -153,21 +180,23 @@ while(j){
 
     // work here
     i = 100000;
-    j=0;
+    j = 0;
     while (i)
     {
         i--;
-        _[j]=(alloc((100000-i)%83 + 3));
-        if(i%13)
-        afree(_[j]);
-        else{
-          j++;
+        _[j] = (alloc((100000 - i) % 83 + 3));
+        if (i % 13)
+            afree(_[j]);
+        else
+        {
+            j++;
         }
     }
-while(j){
-  j--;
-  afree(_[j]);
-}
+    while (j)
+    {
+        j--;
+        afree(_[j]);
+    }
 
     printf(" took %f seconds to execute \n", TTIME(t));
     return 0;
