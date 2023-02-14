0386a1ef70a8cbfd01276028daab537f6a254157
iudah <olaolujude@gmail.com>
2023-02-15 00:15:58 +0100

    Using segregated list for blocks

--- mem.c
+++ mem.c
@@ -2,6 +2,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "mem.h"
 
 typedef struct blk_ blk_t;
@@ -23,8 +24,14 @@ struct blk_
 #define CAN_SPLIT(b, sz) ((((blk_t *)b)->head.size - sz) >= MIN_SIZE)
 #define GET_HEAD(b) ((blk_t *)(((char *)b) - sizeof(struct head_)))
 #define GET_SUCCESSIVE_HEAD(b) ((blk_t *)((&((blk_t *)b)->ptr) + b->head.size))
-#define CAN_MERGE(b) (freelist && (GET_SUCCESSIVE_HEAD(b) != limit) && (GET_SUCCESSIVE_HEAD(b)->head.in_use == 0))
+#define CAN_MERGE(b) ((list_index >= 0) && (GET_SUCCESSIVE_HEAD(b) != limit) && (GET_SUCCESSIVE_HEAD(b)->head.in_use == 0))
 #define PREV(b) (((blk_t **)(&((blk_t *)(b))->ptr))[0])
+#define LIST_INDEX(sz) (sz / sizeof(void *) - 1)
+
+#define FREELIST segregated_list[list_index]
+#define FREETOP segregated_tops[list_index]
+#define LAST_FIT segregated_last_fits[list_index]
+
 #define FIRST_FIT 0
 #define BEST_FIT 01
 #define NEXT_FIT 0
@@ -35,9 +42,11 @@ struct blk_
 // blk_t *heap_start = 0;
 // blk_t *top;
 
-blk_t *freelist = 0;
-blk_t *freetop = 0;
-blk_t *last_fit = 0;
+int list_index = -1;
+
+blk_t *segregated_list[17];
+blk_t *segregated_tops[17];
+blk_t *segregated_last_fits[17];
 
 void *limit = 0;
 
@@ -54,17 +63,17 @@ static void *get_mem_chunk(size_t sz)
 }
 static inline void adjust_free_list(blk_t *b, blk_t *prev, blk_t *next)
 {
-    if (b == freelist)
+    if (b == FREELIST)
     {
-        freelist = prev;
-        if (!freelist)
-            freelist = next;
+        FREELIST = prev;
+        if (!FREELIST)
+            FREELIST = next;
     }
-    if (b == freetop)
+    if (b == FREETOP)
     {
-        freetop = next;
-        if (!freetop)
-            freetop = prev;
+        FREETOP = next;
+        if (!FREETOP)
+            FREETOP = prev;
     }
 }
 static inline void *fix_free_list(blk_t *b)
@@ -88,24 +97,24 @@ static inline void *fix_free_list(blk_t *b)
 }
 static void *reuse_block(size_t size)
 {
-    if (!freelist)
+    if (!FREELIST)
         return 0;
     if (FIRST_FIT)
     {
-        struct head_ *b = (struct head_ *)freelist;
+        struct head_ *b = (struct head_ *)FREELIST;
         while (b)
         {
             if (b->size >= size && !b->in_use)
             {
                 // b->in_use = 1;
-                return fix_free_list((blk_t*)b);
+                return fix_free_list((blk_t *)b);
             }
             b = (struct head_ *)b->next;
         }
     }
     else if (BEST_FIT)
     {
-        struct head_ *b = (struct head_ *)freelist,
+        struct head_ *b = (struct head_ *)FREELIST,
                      *res = 0;
         while (b)
         {
@@ -113,7 +122,7 @@ static void *reuse_block(size_t size)
             {
                 if (b->size == size)
                 {
-                    return fix_free_list((blk_t*)b);
+                    return fix_free_list((blk_t *)b);
                 }
                 if (res ? res->size > b->size : 1)
                     res = b;
@@ -122,22 +131,22 @@ static void *reuse_block(size_t size)
         }
         if (res)
         {
-            return fix_free_list((blk_t*)res);
+            return fix_free_list((blk_t *)res);
         }
         return res;
     }
     else if (NEXT_FIT)
     {
-        struct head_ *b = (struct head_ *)(last_fit ? last_fit : freelist);
+        struct head_ *b = (struct head_ *)(LAST_FIT ? LAST_FIT : FREELIST);
         while (b)
         {
             if (b->size >= size && !b->in_use)
             {
-                last_fit = b->next;
-                return fix_free_list((blk_t*)b);
+                LAST_FIT = b->next;
+                return fix_free_list((blk_t *)b);
             }
-            b = (struct head_ *)(b->next ? b->next : freelist);
-            if ((b == ((struct head_ *)freelist) && last_fit == 0) || ((blk_t *)b) == last_fit)
+            b = (struct head_ *)(b->next ? b->next : FREELIST);
+            if ((b == ((struct head_ *)FREELIST) && LAST_FIT == 0) || ((blk_t *)b) == LAST_FIT)
             {
                 return 0;
             }
@@ -153,11 +162,23 @@ static void split(blk_t *b, size_t sz)
     bb->head.in_use = 1;
     afree(&bb->ptr);
 }
+static inline void set_list_index(size_t sz)
+{
+    list_index = LIST_INDEX(sz);
+    list_index = list_index > 16 ? 16 : list_index;
+}
 static void *alloc(size_t sz)
 {
     if (!sz)
         return 0;
     sz = ALIGN(sz);
+    if (list_index < 0)
+    {
+        memset(segregated_list, 0, sizeof(segregated_list));
+        memset(segregated_tops, 0, sizeof(segregated_tops));
+        memset(segregated_last_fits, 0, sizeof(segregated_last_fits));
+    }
+    set_list_index(sz);
     // printf("alloc %zu\n", sz);
     blk_t *b;
     if ((b = reuse_block(sz)))
@@ -204,25 +225,24 @@ static void afree(void *b)
     top = b;
     b->head.next = 0;*/
 
-        if (!freelist)
+        if (!FREELIST)
         {
-            freelist = h;
+            FREELIST = h;
         }
         else
         {
-            freetop->head.next = h;
+            FREETOP->head.next = h;
         }
 
-        PREV(h) = freetop;
+        PREV(h) = FREETOP;
         //_ = PREV(h);
-        freetop = h;
+        FREETOP = h;
         h->head.next = 0;
     }
 }
 int main()
 {
-
-    /*void *b = alloc(3);
+    void *b = alloc(3);
     afree(b);
     void *c = alloc(25);
     afree(c);
@@ -230,7 +250,7 @@ int main()
     afree(b);
     c = alloc(20);
     afree(c);
-    //exit(9);*/
+    exit(9);
     // printf("%i ; %zu\n", 3, get_head(b)->head.sz);
 
     clock_t t;
